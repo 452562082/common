4422a73a6ca1dc885fd6525e47ed7f75d9f57bc1
liuxp <liuxp_0325@qq.com>
2017-06-30 10:05:37 +0800

    测试hbase

--- hbase/hbase_test.go
+++ hbase/hbase_test.go
@@ -1,6 +1,7 @@
 package hbase
 
 import (
+	"encoding/binary"
 	"git.apache.org/thrift.git/lib/go/thrift"
 	"git.oschina.net/kuaishangtong/common/utils/log"
 	"testing"
@@ -11,10 +12,10 @@ var (
 	host   string = "127.0.0.1"
 	port   string = "9090"
 	table  string = "asv_log_info"
-	rowkey string = "row_214718asjdk812"
+	rowkey string = "rowkey1111"
 )
 
-func TestNewTHBaseServiceClientFactory(t *testing.T) {
+func TestNewTHBaseServiceExists(t *testing.T) {
 
 	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
 	transport, err := thrift.NewTSocketTimeout(host+":"+port, 10*time.Second)
@@ -38,4 +39,67 @@ func TestNewTHBaseServiceClientFactory(t *testing.T) {
 	}
 
 	log.Infof("rowkey{%s} in table{%s} Exists:%t\n", rowkey, table, isexists)
+}
+
+func TestNewTHBaseServiceGet(t *testing.T) {
+
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	transport, err := thrift.NewTSocketTimeout(host+":"+port, 10*time.Second)
+	if err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	log.Debug(transport.Conn())
+
+	client := NewTHBaseServiceClientFactory(transport, protocolFactory)
+	if err := transport.Open(); err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	value, err := client.Get([]byte(table), &TGet{Row: []byte(rowkey)})
+	if err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	log.Infof("rowkey{%s} in table{%s} value :%s", rowkey, table, value.String())
+}
+func TestNewTHBaseServiceScan(t *testing.T) {
+
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	transport, err := thrift.NewTSocketTimeout(host+":"+port, 10*time.Second)
+	if err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	log.Debug(transport.Conn())
+
+	client := NewTHBaseServiceClientFactory(transport, protocolFactory)
+	if err := transport.Open(); err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	startrow := make([]byte, 4)
+	binary.LittleEndian.PutUint32(startrow, 1)
+	stoprow := make([]byte, 4)
+	binary.LittleEndian.PutUint32(stoprow, 10)
+
+	r, err := client.GetScannerResults([]byte(table), &TScan{
+		StartRow: startrow,
+		StopRow:  stoprow,
+	}, 100)
+
+	if err != nil {
+		log.Error(err)
+		t.Fatal(err)
+	}
+
+	for _, v := range r {
+		log.Infof("scan in table{%s} %s", table, v.String())
+	}
+
 }
