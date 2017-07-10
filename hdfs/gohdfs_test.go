5345200f727f9bd179a20e7cd2913dfd3306fb40
liuxp <liuxp0827@foxmail.com>
2017-07-10 21:15:41 +0800

    11

--- hdfs/gohdfs_test.go
+++ hdfs/gohdfs_test.go
@@ -6,10 +6,6 @@ import (
 )
 
 func TestHdfs(t *testing.T) {
-	client, err := hdfs.New("izwz9jay6aqdkr9udoehckz:9000")
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	usrname, err := hdfs.Username()
 	if err != nil {
@@ -17,6 +13,11 @@ func TestHdfs(t *testing.T) {
 	}
 
 	t.Log(usrname)
+
+	client, err := hdfs.NewForUser("127.0.0.1:9000", usrname)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	fw, err := client.Create("test.txt")
 	if err != nil {
