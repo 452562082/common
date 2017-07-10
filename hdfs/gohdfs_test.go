3b45d8bdf0d5d52c22ceaa050adadc6edfb3e8fb
liuxp <liuxp0827@foxmail.com>
2017-07-10 21:16:40 +0800

    11

--- hdfs/gohdfs_test.go
+++ hdfs/gohdfs_test.go
@@ -6,13 +6,10 @@ import (
 )
 
 func TestHdfs(t *testing.T) {
-
 	usrname, err := hdfs.Username()
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	t.Log(usrname)
 
 	client, err := hdfs.NewForUser("127.0.0.1:9000", usrname)
 	if err != nil {
