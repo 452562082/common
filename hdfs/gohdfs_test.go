e9fd70433a7af557f1f93d1e190b466c356e82b3
liuxp <liuxp0827@foxmail.com>
2017-07-11 17:24:31 +0800

    11

--- hdfs/gohdfs_test.go
+++ hdfs/gohdfs_test.go
@@ -1,7 +1,6 @@
 package hdfs
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -11,14 +10,8 @@ func TestHdfs(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = client.client.MkdirAll("/models/", 0777)
-
-	for i := 0; i < 100; i++ {
-		err = client.WriteFile(fmt.Sprintf("/models/m_%05d.ark", i), []byte("test test test"))
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+	fi, err := client.client.Stat("/models")
+	t.Log(fi.Name())
 }
 
 func TestHdfsCopyAllToLocal(t *testing.T) {
