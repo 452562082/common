d695ed1a31811f149f1de66573d3b2a672d6f508
liuxp <liuxp_0325@qq.com>
2017-07-16 10:52:55 +0800

    11

--- hdfs/gohdfs.go
+++ hdfs/gohdfs.go
@@ -2,6 +2,7 @@ package hdfs
 
 import (
 	"fmt"
+	"git.oschina.net/kuaishangtong/common/utils/httplib"
 	"git.oschina.net/kuaishangtong/common/utils/log"
 	"github.com/colinmarc/hdfs"
 	"io/ioutil"
@@ -12,29 +13,153 @@ import (
 )
 
 type HdfsClient struct {
-	client *hdfs.Client
+	conn_hdfs_addr string
+	client         *hdfs.Client
+	host_web_map   map[string]string
+	web_host_map   map[string]string
 }
 
 var defaultHdfsClient *HdfsClient
 
-func Init(address string) error {
+type NameNodeStatus struct {
+	Beans []struct {
+		State string `json:"State"`
+	} `json:"beans"`
+}
+
+func Init(hdfs_addr, hdfs_http_addr []string) error {
 	var err error
-	defaultHdfsClient, err = NewHdfsClient(address)
+
+	defaultHdfsClient, err = NewHdfsClient(hdfs_addr, hdfs_http_addr)
 	if err != nil {
 		return err
+
 	}
 	return nil
 }
 
-func NewHdfsClient(address string) (*HdfsClient, error) {
-	client, err := hdfs.New(address)
+func CheckHDFSAlive(hdfs_web_host string) (bool, error) {
+	var nnStatus NameNodeStatus
+	err := httplib.Get(hdfs_web_host + "/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus").ToJson(&nnStatus)
+	if err != nil {
+		return false, err
+	}
+
+	if len(nnStatus.Beans) < 1 {
+		return false, fmt.Errorf("bean in NameNodeStatus is nil")
+	}
+
+	state := nnStatus.Beans[0].State
+
+	return state == "active", nil
+}
+
+func NewHdfsClient(hdfs_addrs, hdfs_http_addrs []string) (*HdfsClient, error) {
+
+	if len(hdfs_addrs) == 0 {
+		return nil, fmt.Errorf("hdfs host array length can not be 0")
+	}
+
+	if len(hdfs_addrs) != len(hdfs_http_addrs) {
+		return nil, fmt.Errorf("hdfs host array length must be equal to hdfs http address array lentgh")
+	}
+
+	var host_web_map map[string]string = make(map[string]string)
+	var web_host_map map[string]string = make(map[string]string)
+
+	for _, http_addr := range hdfs_http_addrs {
+		for _, addr := range hdfs_addrs {
+			if strings.Split(http_addr, ":")[0] == strings.Split(addr, ":")[0] {
+				host_web_map[addr] = http_addr
+				web_host_map[http_addr] = addr
+			}
+		}
+	}
+
+	log.Info(host_web_map)
+
+	var err error
+	var client *hdfs.Client
+	var addr string
+
+	for _, addr = range hdfs_addrs {
+		client, err = hdfs.New(addr)
+		if err != nil {
+			log.Errorf("hdfs connect to %s failed: %v", addr, err)
+			continue
+		}
+		goto END
+	}
+
+END:
 	if err != nil {
 		return nil, err
 	}
 
-	return &HdfsClient{
-		client: client,
-	}, nil
+	hclient := &HdfsClient{
+		conn_hdfs_addr: addr,
+		client:         client,
+		host_web_map:   host_web_map,
+		web_host_map:   web_host_map,
+	}
+
+	go hclient.checkLoop(hdfs_http_addrs)
+
+	return hclient, nil
+}
+
+func (hc *HdfsClient) checkLoop(hdfs_addrs []string) {
+	var alive map[string]struct{} = make(map[string]struct{})
+
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			for _, v := range hdfs_addrs {
+				active, err := CheckHDFSAlive(v)
+				if err != nil {
+					log.Error(err)
+					goto NEXT
+				}
+
+				if active {
+					alive[v] = struct{}{}
+				} else {
+					delete(alive, v)
+				}
+
+			NEXT:
+			}
+
+			if _, ok := alive[hc.host_web_map[hc.conn_hdfs_addr]]; !ok && len(alive) > 0 {
+				for http_addr, _ := range alive {
+					err := hc.ResetHDFSConnection(hc.web_host_map[http_addr])
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+					log.Infof("Reconnect to HDFS %s success", hc.web_host_map[http_addr])
+					goto END
+				}
+			}
+		END:
+		}
+	}
+}
+
+func (hc *HdfsClient) ResetHDFSConnection(hdfsaddr string) error {
+	var err error
+
+	hc.client.Close()
+
+	hc.client, err = hdfs.New(hdfsaddr)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (hc *HdfsClient) WriteFile(filename string, data []byte) error {
@@ -86,15 +211,6 @@ func GetWaveFromHDFS(hdfsfile string) ([]byte, error) {
 
 func CopyModelFromHDFS(hdfsfile string) error {
 	return defaultHdfsClient.CopyFileToLocal(hdfsfile, hdfsfile)
-}
-
-func GetWaveFromHDFS2(hdfsaddr, hdfsfile string) ([]byte, error) {
-	client, err := NewHdfsClient(hdfsaddr)
-	if err != nil {
-		return nil, err
-	}
-
-	return client.ReadFile(hdfsfile)
 }
 
 func (hc *HdfsClient) ReadFile(filename string) ([]byte, error) {
@@ -264,5 +380,87 @@ func checkPath(path string) error {
 	if !strings.HasPrefix(path, "/") {
 		return fmt.Errorf("filename must be a absolute path")
 	}
+	return nil
+}
+
+func InitSyncModel(hdfs_addr, hdfs_http_addr []string, modeldir string) error {
+
+	if modeldir[len(modeldir)-1] != '/' {
+		modeldir += "/"
+	}
+	hdfsClient, err := NewHdfsClient(hdfs_addr, hdfs_http_addr)
+	if err != nil {
+		return err
+	}
+
+	var localmap, hdfsmap map[string]struct{} = make(map[string]struct{}), make(map[string]struct{})
+
+	local_file_infos, err := ioutil.ReadDir(modeldir)
+	if err != nil {
+		return err
+	}
+
+	for i, v := range local_file_infos {
+		if i < 5 {
+			log.Debug(modeldir + v.Name())
+		}
+		localmap[modeldir+v.Name()] = struct{}{}
+	}
+
+	log.Infof("catch local ivfiles, count: %d", len(local_file_infos))
+
+	hdfs_file_infos, err := hdfsClient.ReadDir(modeldir)
+	if err != nil {
+		return err
+	}
+
+	for i, v := range hdfs_file_infos {
+		if i < 5 {
+			log.Debug(modeldir + v.Name())
+		}
+		hdfsmap[modeldir+v.Name()] = struct{}{}
+	}
+
+	log.Infof("catch hdfs ivfiles, count: %d", len(hdfs_file_infos))
+
+	for k, _ := range hdfsmap {
+
+		if _, ok := localmap[k]; ok {
+			delete(localmap, k)
+		}
+
+		delete(hdfsmap, k)
+	}
+
+	download := len(hdfsmap)
+
+	log.Infof("%d need to download to local", download)
+
+	delete := len(localmap)
+
+	log.Infof("%d need to delete", delete)
+
+	for k, _ := range localmap {
+		err := os.Remove(k)
+		if err != nil {
+			log.Error(err)
+		}
+		log.Debugf("remove %s", k)
+	}
+
+	for k, _ := range hdfsmap {
+		err := hdfsClient.CopyFileToLocal(k, k)
+		if err != nil {
+			log.Error(err)
+		}
+
+		log.Debugf("download %s", k)
+	}
+
+	err = hdfsClient.Close()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hdfs/gohdfs_test.go
+++ hdfs/gohdfs_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestHdfsStat(t *testing.T) {
-	client, err := NewHdfsClient("192.168.1.185:9000")
+	client, err := NewHdfsClient([]string("192.168.1.185:9000"), []string("192.168.1.185:50070"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -14,28 +14,37 @@ func TestHdfsStat(t *testing.T) {
 	t.Log(fi.Name())
 }
 
-func TestHdfsCopyAllToLocal(t *testing.T) {
-	client, err := NewHdfsClient("localhost:9000")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = client.CopyAllFilesToLocal("/models/", "/tmp")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-}
-
-func TestHdfsCopyAllToRemote(t *testing.T) {
-	client, err := NewHdfsClient("localhost:9000")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = client.CopyAllFilesToRemote("/usr/local/gowork/src/git.oschina.net/kuaishangtong/common/hdfs/wave", "/wave")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-}
+//func TestHdfsAlive(t *testing.T) {
+//	active, err := CheckHDFSAlive("http://39.108.128.51:50070")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	t.Log(active)
+//
+//}
+//
+//func TestHdfsCopyAllToLocal(t *testing.T) {
+//	client, err := NewHdfsClient([]string("192.168.1.185:9000"), []string("192.168.1.185:50070"))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	err = client.CopyAllFilesToLocal("/models/", "/tmp")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//}
+//
+//func TestHdfsCopyAllToRemote(t *testing.T) {
+//	client, err := NewHdfsClient([]string("192.168.1.185:9000"), []string("192.168.1.185:50070"))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	err = client.CopyAllFilesToRemote("/usr/local/gowork/src/git.oschina.net/kuaishangtong/common/hdfs/wave", "/wave")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//}
