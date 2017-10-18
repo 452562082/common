056c467f6cd3e04acbef3d94b7bcc5f9114ab608
liuxp <liuxp0827@foxmail.com>
2017-10-18 17:24:44 +0800

    11

--- elastic/goelastic.go
+++ elastic/goelastic.go
@@ -208,7 +208,7 @@ func (ec *ElasticClient) WildcardQuery(index, typ string, key, value string) (*e
 		Index(index).
 		Type(typ). // search in index "twitter"
 		Query(q).  // use wildcard query defined above
-		Size(-1).
+		Size(10000000).
 		Do() // execute
 	if err != nil {
 		return nil, err
