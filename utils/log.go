fbb80521a35a6afd49153923c6da882e3275b394
liuxp <liuxp0827@foxmail.com>
2017-11-24 16:52:12 +0800

    11

---
+++ utils/log.go
@@ -0,0 +1,17 @@
+package utils
+
+import "git.oschina.net/kuaishangtong/common/utils/log"
+
+type ErrLog struct {
+}
+
+func (e *ErrLog) Printf(format string, v ...interface{}) {
+	log.Errorf(format, v...)
+}
+
+type InfoLog struct {
+}
+
+func (e *InfoLog) Printf(format string, v ...interface{}) {
+	log.Errorf(format, v...)
+}
