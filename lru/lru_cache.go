0566edae19de2b3048febc881165cee38cccaec8
liuxiangpeng <liuxp827@foxmail.com>
2018-06-23 16:26:34 +0800

    11

--- lru/lru_cache.go
+++ lru/lru_cache.go
@@ -4,7 +4,7 @@ import (
 	"container/list"
 	"encoding/binary"
 	"fmt"
-	"goal/github.com/coocood/freecache"
+	"github.com/coocood/freecache"
 	"kuaishangtong/common/utils/log"
 	"sync"
 	"time"
@@ -109,8 +109,7 @@ func (c *CacheMem) SetTimeout(timeout int64) {
 func (c *CacheMem) cacheSearch(key Key) (e *list.Element, ok bool) {
 	keybytes := []byte(key.(string))
 
-	var valueBuf []byte = make([]byte, 64)
-	valuebytes, _ := c.cache.GetToBuf(keybytes, valueBuf)
+	valuebytes, _ := c.cache.Get(keybytes)
 	if valuebytes != nil {
 		e = (*list.Element)(unsafe.Pointer(uintptr(binary.LittleEndian.Uint64(valuebytes))))
 		ok = true
